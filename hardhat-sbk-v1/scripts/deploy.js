509401eaedc68c941a0e7f7f11766331baf1af25
TMcNeillEY <Tiquila.McNeill@ey.com>
2023-01-20 16:30:35 -0500

    first commit

---
+++ hardhat-sbk-v1/scripts/deploy.js
@@ -0,0 +1,33 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
+
+async function main() {
+  // Address of the whitelist contract that you deployed in the previous module
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // URL from where we can extract the metadata for a DET SBK NFT
+  const metadataURL = METADATA_URL;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so DETSBK Contract here is a factory for instances of our DET SBK contract.
+  */
+  const detSbkNftDevsContract = await ethers.getContractFactory("Dev");
+
+  // deploy the contract
+  const deployedDetSbkNftDevsContract = await detSbkNftDevsContract.deploy(
+  );
+
+  // print the address of the deployed contract
+  console.log(
+    "DET SBK Devs Contract Address:",
+    deployedDetSbkNftDevsContract.address
+  );
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
